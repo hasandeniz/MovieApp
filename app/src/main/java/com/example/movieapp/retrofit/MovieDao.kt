4d1660bc9ad8b3d259658a054f839116653bbbb9
hasandeniz <hasandeniz394@gmail.com>
2022-11-28 16:35:14 +0300

    changed database structure

--- app/src/main/java/com/example/movieapp/retrofit/MovieDao.kt
+++ app/src/main/java/com/example/movieapp/retrofit/MovieDao.kt
@@ -1,6 +1,5 @@
 package com.example.movieapp.retrofit
 
-import com.example.movieapp.BuildConfig
 import com.example.movieapp.data.model.MovieDetailsResponse
 import com.example.movieapp.data.model.MoviesResponse
 import retrofit2.http.GET
@@ -8,10 +7,10 @@ import retrofit2.http.Query
 
 interface MovieDao {
 
-    @GET("?apikey=${BuildConfig.API_KEY}")
+    @GET("?apikey=37ad3207")
     suspend fun searchMovieByTitle(@Query("s") Title: String, @Query("page") Page: Int): MoviesResponse
 
-    @GET("?apikey=${BuildConfig.API_KEY}")
+    @GET("?apikey=37ad3207")
     suspend fun getMovieDetailsByImdbIdFromApi(@Query("i") imdbId: String): MovieDetailsResponse
 
 }
